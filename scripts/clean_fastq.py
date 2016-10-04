99d20ffa707bd9ce57d0a2faf83bd0b34460c88c
killidude <hrbek@evoamazon.net>
2016-10-04 08:17:33 -0400

    script to remove short fastq sequences from a file

---
+++ scripts/clean_fastq.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+#######################################
+# A simple script to remove short sequences from a FASTQ file
+#
+# FASTQ cleanup
+#
+# Author:  Tomas Hrbek
+# Email: hrbek@evoamazon.net
+# Date: 26.06.2015
+# Version: 1.0
+#######################################
+
+import argparse
+from Bio import SeqIO
+
+__author__ = 'legal'
+
+ 
+parser = argparse.ArgumentParser(description='Script to remove short sequences from a fastq file.')
+parser.add_argument('input', help='input file name')
+parser.add_argument('output', help='output file name')
+parser.add_argument('cut_off', help='minimum fragment length, default 250 bp', type=int, nargs='?', default=250)
+args = parser.parse_args()
+    
+n_reads = 0 #Initialize rad counter
+sequences = [] #Setup an empty list of sequence lengths
+
+input_handle = open(args.input, "rU")
+output_handle = open(args.output, "w")
+
+reads_iterator = SeqIO.parse(input_handle, "fastq")
+seqs_iterator = (reads for reads in reads_iterator if len(reads) >= args.cut_off)
+
+SeqIO.write(seqs_iterator, output_handle, "fastq")
+output_handle.close()
+
+
+## show values ##
+print("Input file: {}".format(args.input))
+print("Output file: {}".format(args.output))
+print("Minimum read length: {}".format(args.cut_off))
