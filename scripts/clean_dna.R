2c6e3f0a681a58b8d4a2808f75406d71ea8b3fbf
rupert@jau <rupertcollins@gmail.com>
2018-01-15 23:24:43 +0000

    added a new DNA cleaning function

--- scripts/clean_dna.R
+++ scripts/clean_dna.R
@@ -4,8 +4,8 @@
 # "N", "-", "?", "R", "Y", etc
 # script gives a warning if these characters are inside the sequences, ie, are not alignment padding chars at the ends
 
-require("ape")
-require("stringr")
+#require("ape")
+#require("stringr")
 
 clean_dna <- function(dna){#
     # convert to a list
@@ -29,3 +29,38 @@ clean_dna <- function(dna){#
     dbin <- as.DNAbin(dlimp)
     return(as.list(dbin))
 }#
+
+
+
+# another cleaning function
+# slightly different - it can remove Ns from only the ends of the sequences, and you have to tell the function what you want to delete
+# example:
+# rm_missing(dna=cleantest, chars=c("?","-","n"), endOnlyNs=TRUE, print=TRUE)
+# it's case insensitive too
+
+rm_missing <- function(dna,chars,endOnlyNs,print){
+# check if object is a DNAbin
+if(class(dna)!="DNAbin"){stop("Please convert your data to a DNAbin object first")} else{
+        dna.c <- as.character(as.list(dna))#convert dna to list and character
+        dna.f <- sapply(dna.c, paste, collapse="")# cat dna
+        chars[str_which(string=chars,pattern="\\?")] <- "\\?"# escape the question mark
+            if(any(!str_detect(string=dna.f, pattern=regex("a|c|t|g", ignore_case=TRUE)))) {# finds sequences with no ACTGs
+            warning("Some of the sequences (rows) have no ACTG nucleotides. I removed these and continued, therefore the length of the output will not be the same as the input.", call.=FALSE)
+            dna.f <- dna.f[str_detect(string=dna.f, pattern=regex("a|c|t|g", ignore_case=TRUE))]# remove those and continue
+            }
+            if(endOnlyNs==TRUE) {
+            chars[str_which(string=chars,pattern="n|N")] <- "%"# replace the n char with an % to remove the problem with empty vectors
+            chars.l <- paste(chars, collapse="|")# create a list of chars
+            chars.l <- paste(chars.l,"^n*|n*$",sep="|")
+            } else {
+                chars.l <- paste(chars, collapse="|")# create a list of chars
+                }
+        reg <- regex(pattern=chars.l, ignore_case=TRUE)# turn into a regex
+        dna.r <- str_replace_all(string=dna.f, pattern=reg, replacement="")# replace the string
+        names(dna.r) <- names(dna.f)# rename
+        if(print==TRUE){print(as.list(dna.r))}
+        dna.r <- as.DNAbin(str_split(string=dna.r, pattern=""))# convert back to a DNAbin
+        names(dna.r) <- names(dna.f)# rename
+        return(dna.r)
+    }
+}
