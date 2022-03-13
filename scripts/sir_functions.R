725880e5c5d13218a5a656cbdf2e41e4d2d03a8e
killidude <hrbek@evoamazon.net>
2022-03-13 12:02:58 -0500

    initial sir functions commit

---
+++ scripts/sir_functions.R
@@ -0,0 +1,208 @@
+#!/usr/bin/env Rscript
+
+#------------------------------
+# deterministic SIR function
+sir_det <- function(N, t, b, a){
+  sir_m <- matrix(nrow = t, ncol = 3) #make empty matrix
+  
+  colnames(sir_m) <- c("S", "I", "R") # assign names to columns
+  rownames(sir_m) <- c(1:t)
+  
+  #initialize matrix with values of first iteration
+  sir_m[1,1] <- N-1 # S = susceptible
+  sir_m[1,2] <- 1 # I = infected
+  sir_m[1,3] <- 0 # R = recovered
+  
+  #b is rate of infection
+  #a is rate of recovery
+  
+  for (i in 2:t){
+    if (sir_m[i-1,2] <= b*sir_m[i-1,2]*sir_m[i-1,3]){ #if # newly infected > susceptible (WHY *sir_m[i-1,3])
+      # S = 0 if # newly infected individuals > S in the previous iteration
+      sir_m[i,2] <- 0
+      # I = I in the previous timestep + S in the previous iteration - newly recovered individuals in the previous iteration
+      sir_m[i,3] <- sir_m[i-1,3] + sir_m[i-1,2] - a*sir_m[i-1,3]
+    }
+    else{
+      # S = S in the previous iteration - newly infected individuals in the previous iteration
+      sir_m[i,2] <- sir_m[i-1,2] - b*sir_m[i-1,2]*sir_m[i-1,3]
+      # I = I in the previous timestep + newly infected individuals in the previous iteration - newly recovered individuals in the previous iteration
+      sir_m[i,3] <- sir_m[i-1,3] + b*sir_m[i-1,2]*sir_m[i-1,3] - a*sir_m[i-1,3]
+    }
+    # R = R in the previous iteration + newly recovered individuals in the previous iteration
+    sir_m[i,4] <- sir_m[i-1,4] + a*sir_m[i-1,3]
+    sir_m[i,1] <- i
+  }
+  return(as.data.frame(sir_m))
+}
+
+
+#------------------------------
+# SIR function that does the actual stochastic simulations
+SIR <- function(n, t, b, a) {
+  # n - number of individuals
+  # t - length (iteration/time) of run
+  # b - infection rate
+  # a - recovery rate
+  
+  # create and initialize matrix
+  dm <- matrix(nrow = t, ncol = n)
+  dm[1,1] <- "I"  # sets the first infected individuals
+  dm[1,2:n] <- "S" # all others are susceptible
+  
+  # loop through time t (rows)
+  for (i in 2:t) {
+    # count number of infected individuals
+    inf <- sum(dm[i-1,] == "I")
+    # loop through individuals (columns)
+    for (j in 1:n) {
+      # if individual is susceptible can become infected or stays susceptible
+      if (dm[i-1,j] == "S") {
+        if (runif(1) <= (1-(1-b)^inf)) {
+          dm[i,j] <- "I"
+        }
+        else {dm[i,j] <- dm[i-1,j] # S
+        }
+      }
+      # else if individual is infected can become removed or stays infected
+      else if (dm[i-1,j] == "I") {
+        if(runif(1) <= b) {
+          dm[i,j] <- "R"
+        }
+        else {
+          dm[i,j] <- dm[i-1,j]	
+        }
+      }
+      # else if individual is removed stays removed
+      else if (dm[i-1,j] == "R") {
+        dm[i,j] <- dm[i-1,j]
+      }
+    }
+  }
+  return(as.data.frame(dm))
+}
+
+
+#------------------------------
+# the stochastic SIR function
+sir_stoch <- function(N, t, b, a) {
+  df <- SIR(N, t, b, a)
+  sir_m <- matrix(ncol=3, nrow=t)
+  colnames(sir_m) <- c("S","I","R")
+  rownames(sir_m) <- c(1:t)
+  
+  for (i in 1:t){
+    sir_m[i, "S"] <- sum((df[i, ] == "S"))
+    sir_m[i, "I"] <- sum((df[i, ] == "I"))
+    sir_m[i, "R"] <- sum((df[i, ] == "R"))
+  }
+  return(as.data.frame(sir_m))
+}
+
+
+#------------------------------
+# plot results of one SIR run
+plot_sir <- function(df) {
+  # takes input matrix with cols of iterations, S, I, R
+  # and creates a plot with all three lines
+  # S = black, I = red, R = green
+  t <- c(1:nrow(df))
+  matplot(t, df$S, xlab='iterations', ylab='number', type='l', col=1, ylim = c(-1,max(df$S)))
+  matlines(t, df$I, col=2)
+  matlines(t, df$R, col=3)
+}
+
+
+#------------------------------
+# plot additional SIR runs, adding to an existing plot
+add_plot_sir <- function(df) {
+  # overlay additional SIR simulations onto an existing plot
+  t <- c(1:nrow(df))
+  matlines(t, df$S, col=1)
+  matlines(t, df$I, col=2)
+  matlines(t, df$R, col=3)
+}
+
+
+------------------------------
+# plot results of one stochastic SIR run
+  
+multi_plot_sir <- function(N, t, b, a, reps) {
+  # run x number of stochastic SIR simulations and plot
+  plot_SIR(sir_stoch(N, t, b, a))	
+  for(i in 2:reps) {
+    add_plot_SIR(sir_stoch(N, t, b, a))	
+  }
+}
+
+
+#------------------------------
+#error_plot
+error_plot <- function(df){
+  # takes the output from the super_summary and plots
+  # the S,I,R classes with sd error bars for each timepoint
+  
+  t <- c(1:nrow(df))
+  
+  matplot(t, df$S, xlab='time steps', ylab='number', type='l', col=1, ylim = max(df$S))
+  segments(t, df$S-df$sdS, t, df$S+df$sdS)
+  
+  matlines(t, df$I, col=2)
+  segments(t, df$I-df$sdI, t, df$I+df$sdI, col=2)
+  
+  matlines(t, df$R, col=3)
+  segments(t, df$R-df$sdR, t, df$R+df$sdR, col=3)
+  
+}
+
+
+#------------------------------
+# the super_summary function:
+summarize_sir_stoch <- function(N, t, b, a, reps) {
+  # creates a series of stochasticSIR reps that
+  # are summarized by the summarize_run function and
+  # then loads the S,I,R counts from each timestep into 
+  # separate S/I/R-specific matrices, and then the
+  # S/I/R-specific matrices are used to create a 
+  # super_summary matrix that has the mean and sd
+  # of the counts of S/I/R for each timestep across
+  # all reps
+  
+  # creates separate matrices to keep the SIR values for each timestep and each run
+  S_val <- matrix(nrow=t, ncol=reps)
+  I_val <- matrix(nrow=t, ncol=reps)
+  R_val <- matrix(nrow=t, ncol=reps)
+  
+  # runs a stochastic sim, summarizes it, and loads the values for timesteps of that run into the summary matrices
+  for(i in 1:reps) {
+    df <- sir_stoch(N, t, b, a)
+    #print(i)
+    S_val[, i] <- df[, 1]
+    I_val[, i] <- df[, 2]
+    R_val[, i] <- df[, 3]
+  }
+  
+  # creates an overall summary matrix for the average and stdev for SIR values at each timestep across all runs
+  df <- as.data.frame(matrix(nrow=t, ncol=6))
+  colnames(df) <- c("muS","sdS","muI","sdI","muR","sdR")
+  rownames(df) <- c(1:t)
+  
+  # creates vectors that hold the means/sds for SIR values applying fn going down rows of the S/I/R value matrices
+  S_rowmeans <- apply(S_val, 1, mean)
+  S_rowstdev <- apply(S_val, 1, sd)
+  I_rowmeans <- apply(I_val, 1, mean)
+  I_rowstdev <- apply(I_val, 1, sd)
+  R_rowmeans <- apply(R_val, 1, mean)
+  R_rowstdev <- apply(R_val, 1, sd)
+  
+  # assign the values from the mean/sd vectors into the summary matrix
+    df$muS <- round(S_rowmeans, 1)
+    df$sdS <- round(S_rowstdev, 2)
+    df$muI <- round(I_rowmeans, 1)
+    df$sdI <- round(I_rowstdev, 2)
+    df$muR <- round(R_rowmeans, 1)
+    df$sdR <- round(R_rowstdev, 2)
+    
+  return(df)
+}
+
