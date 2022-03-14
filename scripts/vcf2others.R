54c6ba2ead0b258eaa2163cfcf8a0b3631c19e53
killidude <hrbek@evoamazon.net>
2022-03-14 13:09:52 -0500

    adding vcf_filter_oneSNP; keep only 1 SNP per locus

--- scripts/vcf2others.R
+++ scripts/vcf2others.R
@@ -694,6 +694,35 @@ vcf_filter_multiSNP <- function(vcf) {
 
 
 ################################
+#' @title vcf_filter_oneSNP
+#' @description subsets vcfR format data keeping only 1 SNP per locus
+#' @author Tomas Hrbek February 2022
+#'
+#' @param vcf -> vcfR object
+#' @export nothing
+#' @return subsetted vcfR object
+#'
+#' @details
+#' This function subsets the vcfR object keeping only 1 SNP per locus, returning new vcfR object
+#' The first SNP independent of quality is taken (may mofify this in future)
+#'
+#' @example
+#' vcf_filter_oneSNP(vcf = my_vcf)
+#' vcf_filter_oneSNP(my_vcf)
+#'
+
+vcf_filter_oneSNP <- function(vcf) {
+    # read all loci names in vcf
+    chrom <- getCHROM(vcf)
+    
+    # keep only those loci with 2+ SNPs
+    vcf <- vcf[!duplicated(chrom),]
+    
+    return(vcf)
+}
+
+
+################################
 #' @title vcf_filter_invariant
 #' @description remove invariant loci from vcfR format data
 #' @author Tomas Hrbek February 2022
