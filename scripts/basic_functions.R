98d31c6ceb3177c3d1478e7eb3e7e658bb7283f2
killidude <hrbek@evoamazon.net>
2022-08-18 13:00:46 -0500

    adding lookup table function

--- scripts/basic_functions.R
+++ scripts/basic_functions.R
@@ -1,16 +1,107 @@
 #!/usr/bin/env Rscript
 
-# functions to calculate harmoninc and geometric means
-# Tomas Hrbek July 2019
+# collection of helper functions
+# Tomas Hrbek starting July 2019
 
 
-# function - harmonic mean
-harmonicMean <- function(logLikelihoods) {
-  1/mean(1/logLikelihoods)
-}
-# function geometric mean
-geometricMean <- function(logLikelihoods) {
-  prod(logLikelihoods)^(1/length(logLikelihoods))
+# harmonic mean
+harmonic_mean <- function(x) {
+  x <- x[!is.na(x)]
+  y <- 1/mean(1/x)
+  return(y)
 }
 
-# examples
+# geometric mean
+geometric_mean <- function(x) {
+  x <- x[!is.na(x)]
+  y <- prod(x)^(1/length(x))
+  return(y)
+}
+
+# modal density value
+density_mode <- function(x) {
+  x <- x[!is.na(x)]
+  td <- density(x)
+  max_dens <- which.max(td$y)
+  y <- td$x[max_dens]
+  return(y)
+}
+
+# mode of discrete series
+# mode function from https://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode
+discrete_mode <- function(x) {
+  x <- x[!is.na(x)]
+  ux <- unique(x)
+  tab <- tabulate(match(x, ux)); 
+  y <- ux[tab == max(tab)]
+  return(y)
+}
+
+# remove NAs from a vector
+rm_na <- function(x) {
+  y <- x[!is.na(x)]
+  return(y)
+}
+
+# generate table of samples and barcode sequences from table of samples and barcode names
+recode_by_lookup <- function (df, lookup, ngs = "illumina") {
+  if(!is.data.frame(lookup) && !is.matrix(lookup)) {
+    stop("The lookup table must be a data.frame or matrix")
+  }
+  if(ncol(lookup) != 2) {
+    stop("The lookup table must have two colums")
+  }
+  if(length(unique(lookup[, 1])) < nrow(lookup) ) {
+    stop("Index names in lookup table are not unique")
+  }
+  if(length(unique(lookup[, 2])) < nrow(lookup) ) {
+    stop("Index sequences in lookup table are not unique")
+  }
+  if(!is.data.frame(df)) {
+    stop("The dataframe must be a tibble or data.frame")
+  }
+  if(ncol(df) != 3 && ngs == "illumina") {
+    stop("The Illumina dataframe must have three colums; q5 q7 id")
+  }
+  else if (ncol(df) != 2 && ngs == "ion") {
+    stop("The IonTorrent dataframe must have two colums; bc id")
+  }
+  else if (ncol(df) != 3 && ngs == "ion_seq") {
+    stop("The IonTorrent dataframe must have three colums; id A P1")
+  }
+  
+  # convert tibble to dataframe (otherwise df operations do not work)
+  df <- as.data.frame(df)
+  # remove missing samples
+  df <- df[!is.na(df$id), ]
+  
+  if(ngs == "illumina") {
+    # lookup q5
+    df$Q5n <- df$Q5
+    df$Q5 <- lookup[match(df$Q5, lookup[, 1]), 2]
+    # lookup q7
+    df$Q7n <- df$Q7
+    df$Q7 <- lookup[match(df$Q7, lookup[, 1]), 2]
+    # reorder columns
+    col_order <- c("id", "Q7n", "Q7", "Q5n", "Q5")
+    df <- df[, col_order]
+  }
+  else if(ngs == "ion") {
+    df$bc <- lookup[match(df$bc, lookup[, 1]), 2]
+    # reorder columns
+    col_order <- c("id", "bc")
+    df <- df[, col_order]
+  }
+  else if(ngs == "ion_seq") {
+    # lookup A
+    df$A <- lookup[match(df$A, lookup[, 1]), 2]
+    # lookup P1
+    df$P1 <- lookup[match(df$P1, lookup[, 1]), 2]
+    # reorder columns
+    col_order <- c("id", "A", "P1")
+    df <- df[, col_order]
+  }
+  
+  return(df)
+}
+
