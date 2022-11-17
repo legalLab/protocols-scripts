b1e821c31aed431dd2c732e060222c4e83236152
killidude <hrbek@evoamazon.net>
2022-11-17 11:35:41 -0600

    adding vcf_filter_maf function

--- scripts/vcf2others.R
+++ scripts/vcf2others.R
@@ -1078,7 +1078,53 @@ vcf_filter_hets <- function(vcf, hets = .4) {
     sum(x == 2, na.rm = TRUE)
   })
   
-  vcf <- vcf[hetero/(homo+hetero) < hets,]
+  vcf <- vcf[hetero/(homo+hetero) <= hets,]
+  
+  return(vcf)
+}
+
+
+################################
+#' @title vcf_filter_maf
+#' @description remove loci below MAF threshold from vcfR format data
+#' @author Tomas Hrbek October 2022
+#'
+#' @param vcf -> vcfR object
+#' @export nothing
+#' @return subsetted vcfR object
+#'
+#' @details
+#' This function removes loci below MAF threshold from the vcfR object
+#'
+#' @example
+#' vcf_filter_maf(vcf = my_vcf, maf = .05)
+#' vcf_filter_maf(my_vcf)
+#'
+
+
+vcf_filter_maf <- function(vcf, maf = .05) {
+  # remove NAs from a vector
+  rm_na <- function(x) {
+    y <- x[!is.na(x)]
+    return(y)
+  }
+  
+  vcf <- extract.indels(vcf, return.indels = F)
+  vcf <- vcf[is.biallelic(vcf), ]
+  gt <- extract.gt(vcf, return.alleles = F, convertNA = T)
+  gt[is.na(gt)] <- "NA"
+  gt[gt == "0/0" | gt == "0|0"] <- "0"
+  gt[gt == "1/1" | gt == "1|1"] <- "1"
+  gt[gt == "0/1" | gt == "0|1" | gt == "1/0" | gt == "1|0"] <- "2"
+  
+  alleles_all <- apply(gt, MARGIN = 1, function(x) {
+    2*length(rm_na(x))
+  })
+  allele_rare <- apply(gt, MARGIN = 1, function(x) {
+    sum(x == 2, na.rm = TRUE) + 2*sum(x == 1, na.rm = TRUE)
+  })
+  
+  vcf <- vcf[allele_rare/alleles_all >= maf,]
   
   return(vcf)
 }
