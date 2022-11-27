ee0872d3e16835408a6679cd0280d033b1170f15
killidude <hrbek@evoamazon.net>
2022-11-27 14:46:08 -0600

    adding vcf2treemix function

--- scripts/vcf2others.R
+++ scripts/vcf2others.R
@@ -2,8 +2,9 @@
 
 # functions to convert vcf data to other formats
 # depends on vcfR (should be loaded since vcfR object [vcf file] is passed to these functions)
-# depends on ape to generate nexus file (vcf2nexus, vcf2snapp)
-# depends on dplyr (vcf_filter_multiSNP)
+# depends on dplyr (many functions)
+# depends on ape (vcf2nexus, vcf2snapp)
+# depends on glue (vcf2fineRadStructure, vcf2treemix)
 
 
 ################################
@@ -436,7 +437,7 @@ vcf2genepop <- function (vcf, ind_pop, keep_pop, inc_missing = TRUE,
     gt[gt == "T/T" | gt == "T|T"] <- "0404"
     gt <- t(gt)
     write(paste("pop ", names(pop_list)[i], sep = ""), file = out_file, append = TRUE)
-    write.table(cbind(sep = ',', gt), file = out_file, quote = FALSE, sep = " ", col.names = FALSE, append = TRUE)
+    utils::write.table(cbind(sep = ',', gt), file = out_file, quote = FALSE, sep = " ", col.names = FALSE, append = TRUE)
   }
   
   return(invisible(NULL))
@@ -520,6 +521,81 @@ vcf2bayescan <- function (vcf, ind_pop, keep_pop, inc_missing = TRUE,
     }
   }
   
+  return(invisible(NULL))
+}
+
+
+################################
+#' @title vcf2treemix
+#' @description converts vcfR format data to TreeMix infile
+#' @description in part based on vcfR2migrate function (vcfR package)
+#' @author Tomas Hrbek November 2022
+#'
+#' @param vcf -> vcfR object
+#' @param ind_pop -> population assignment of individuals in vcf (factor)
+#' @param keep_pop -> population(s) of interest to include in TreeMix infile (factor)
+#' @param inc_missing -> include missing data (logical)
+#' @param out_file -> name of file to output (TreeMix infile)
+#' @export TreeMix infile of SNPs
+#' @return nothing
+#'
+#' @details
+#' This function converts the vcfR object to a TreeMix formatted input file
+#' The function will remove indels, and multiallelic loci, and optionally loci with missing data
+#'
+#' @example
+#' vcf2treemix(vcf = my_vcf, ind_pop = ind_pop, keep_pop = keepers, inc_missing = TRUE, out_file = "TreeMix_infile.txt")
+#' vcf2treemix(my_vcf, ind_pop, keepers, out_file = "TreeMix_infile.txt")
+#' vcf2treemix(my_vcf, ind_pop, keepers)
+#'
+
+vcf2treemix <- function (vcf, ind_pop, keep_pop, inc_missing = TRUE, 
+                          out_file = "treemix_infile.txt")
+{
+  if (class(vcf) != "vcfR") {
+    stop(paste("Expecting an object of class vcfR, received a", 
+               class(vcf), "instead"))
+  }
+  if (class(ind_pop) != "factor" | class(keep_pop) != "factor") {
+    stop(paste("Expecting population vector, received a", 
+               class(ind_pop), "and", class(keep_pop), "instead"))
+  }
+  vcf <- extract.indels(vcf, return.indels = F)
+  vcf <- vcf[is.biallelic(vcf), ]
+  if (inc_missing == FALSE) {
+    gt <- extract.gt(vcf, convertNA = T)
+    vcf <- vcf[!rowSums((is.na(gt))), ]
+  }
+  vcf_list <- lapply(keep_pop, function(x) {
+    vcf[, c(TRUE, x == ind_pop)]
+  })
+  names(vcf_list) <- keep_pop
+  
+  for (i in 1:length(vcf_list)) {
+    gt <- extract.gt(vcf_list[[i]], return.alleles = F, convertNA = T) #convertNA not working here
+    allele1 <- apply(gt, MARGIN = 2, function(x) {
+      substr(x, 1, 1)
+    })
+    rownames(allele1) <- c(1:nrow(allele1))
+    allele2 <- apply(gt, MARGIN = 2, function(x) {
+      substr(x, 3, 3)
+    })
+    rownames(allele2) <- c(1:nrow(allele1))
+    
+    REF <- apply(cbind(allele1, allele2), 1, function(x){sum(x == 0, na.rm = TRUE)})
+    ALT <- apply(cbind(allele1, allele2), 1, function(x){sum(x == 1, na.rm = TRUE)})
+    GEN <- apply(cbind(REF, ALT), 1, function(x){glue::glue_collapse(x, sep = ",")})
+    
+    if (i == 1) {
+      df <- GEN
+    } else {
+      df <- cbind(df, GEN)
+    }
+  }
+  
+  colnames(df) <- keep_pop
+  utils::write.table(df, file = out_file, quote = FALSE, sep = " ", col.names = TRUE, row.names = FALSE)
+
   return(invisible(NULL))
 }
 
