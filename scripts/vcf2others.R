ba61528a426b0a63023f02bf4c85218fd5109b63
killidude <hrbek@evoamazon.net>
2023-01-14 09:33:12 -0600

    adding vcf_sub_loci function to randomly subset vcf by locus

--- scripts/vcf2others.R
+++ scripts/vcf2others.R
@@ -1161,6 +1161,37 @@ vcf_sub_indivs <- function(vcf, indiv, whitelist = TRUE) {
 
 
 ################################
+#' @title vcf_sub_loci
+#' @description randomly subsets vcfR format data to # of loci
+#' @author Tomas Hrbek January 2023
+#'
+#' @param vcf -> vcfR object
+#' @param n_loci -> number of loci to subset (numeric)
+#' @export nothing
+#' @return subsetted vcfR object
+#'
+#' @details
+#' This function subsets the vcfR object to specific # of loci, returning new vcfR object
+#'
+#' @example
+#' vcf_sub_loci(vcf = my_vcf, n_loci = n_loci)
+#' vcf_sub_loci(my_vcf, n_loci)
+#'
+
+vcf_sub_loci <- function(vcf, n_loci = 1000) {
+  gt <- extract.gt(vcf, convertNA = T)
+  # get number of snps in vcf
+  n_snps <- nrow(gt)
+  
+  x <- sample(1:n_snps, n_loci)
+  
+  vcf <- vcf[x,]
+  
+  return(vcf)
+}
+
+
+################################
 #' @title vcf_filter_missingness
 #' @description subsets vcfR format data by % missing data
 #' @author Tomas Hrbek February 2022
