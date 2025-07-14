07a23d899987515c8c36968f669cf9854ef06295
killidude <hrbek@evoamazon.net>
2025-07-14 12:23:16 -0500

    removing analyze_edna function

--- scripts/helper_functions.R
+++ scripts/helper_functions.R
@@ -193,39 +193,3 @@ recode_by_lookup <- function(df, lookup, ngs = "novogene", ...) {
   }
   return(df)
 }
-
-# process NCBI local blast+ output
-analyze_edna <- function(infile, path, lookup_table, pid = .97, len = 99, n_reads = 4) {
-  #check if file is not empty first - empty gz files are 61-62 bytes
-  #try(if(file.size(paste0(path, infile)) <= 100L) stop(paste0("empty file: ", infile)))
-  
-  # check if file is not empty first - empty gz files are 61-62 bytes
-  # make a fake file with 1 sample "bob"
-  if(file.size(paste0(path, infile)) <= 100L) {
-    blast_table <- tibble(id = "bob", n = 1, freq = 0, taxonomy = "Bob;Bob;Bob;Bob;Bob;Bob;Bob bob")
-    return(blast_table)
-    break
-  }
-  
-  blast <- read.table(gzfile(paste0(path, infile)), header = FALSE, sep = "\t")
-  colnames(blast) <- c('qacc', 'sacc', 'evalue', 'pident', 'mismatch', 'length', 'qstart', 'qend', 'sstart', 'send')
-  
-  blast <- as_tibble(blast) %>%
-    distinct(qacc, .keep_all = TRUE) %>%
-    mutate(pident = pident/100) %>%
-    filter(pident >= pid & length > len)
-  
-  #blast$sacc <- str_extract(blast$sacc, regex("[a-zA-Z0-9_]+$"))
-  blast_table <- blast %>%
-    mutate(n = as.numeric(str_extract(.$qacc, pattern = "[0-9]+$"))) %>%
-    group_by(sacc) %>%
-    summarise(n = sum(n)) %>%
-    mutate(freq = n/sum(n)) %>%
-    filter(n > n_reads) %>%
-    arrange(desc(n)) %>%
-    rename(id = sacc) %>%
-    left_join(lookup_table, by = 'id') %>%
-    add_row(id = "bob", n = 1, freq = 0, taxonomy = "Bob;Bob;Bob;Bob;Bob;Bob;Bob bob")
-  
-  return(blast_table)
-}
